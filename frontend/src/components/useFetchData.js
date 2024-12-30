7cced7dce04bbeb0363f202bb21dc4a776204016
William Forsyth <williamforsyth@Williams-MacBook-Air.local>
2024-12-30 13:29:34 +0000

    adding components

---
+++ frontend/src/components/useFetchData.js
@@ -0,0 +1,35 @@
+import  { useEffect, useState, useCallback } from "react";
+
+const useFetchData = () => {
+    const [status, setStatus] = useState('idle');
+    const [talks, setTalks]=useState([{
+        id:"",
+        speaker:"",
+        title:"",
+        description:"",
+        session:"",
+        time:"",
+        tags:[],
+        ratings:[],
+        _id:"",
+      }]);
+
+  const fetchData = useCallback(() => {
+    const url = "http://localhost:3001/talks";
+    fetch(url)
+      .then((response) => response.json())
+      .then((incomingData) => {
+        console.log(incomingData)
+        setFoods(incomingData);
+        setStatus('fetched');
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  return { status, talks };
+};
+export default useFetchData;
