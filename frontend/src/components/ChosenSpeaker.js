15190741760c13f65dffc78940ec85604f53dcad
William Forsyth <williamforsyth@Williams-MacBook-Air.local>
2025-01-03 15:28:59 +0000

    custom divider line

--- frontend/src/components/ChosenSpeaker.js
+++ frontend/src/components/ChosenSpeaker.js
@@ -1,21 +1,26 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
 
 const ChosenSpeaker = ({ chosenSpeaker, filteredTalks }) => {
-    return (
-      <div className="row justify-content-center">
-        <div className="col-md-8 col-lg-6 py-4">
-          <ul className='list-unstyled'>
-            {filteredTalks.map((talk, index) => (
-              <li key={index}>
-                <strong>{talk.title}</strong>
-                <p>{talk.session}</p>
-                <p>{talk.time}</p>
-              </li>
-            ))}
-          </ul>
-        </div>
+  return (
+    <div className="row justify-content-center">
+      <div className="col-md-8 col-lg-6 py-4">
+        {filteredTalks.map((talk, index) => (
+          <Card className="mb-3" key={index}>
+            <Card.Body>
+              <Card.Title><strong>{talk.title}</strong></Card.Title>
+              <Card.Text>
+                <strong>Session:</strong> {talk.session}
+              </Card.Text>
+              <Card.Text>
+                <strong>Time:</strong> {talk.time}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        ))}
       </div>
-    );
-  };
-  
-  export default ChosenSpeaker;
+    </div>
+  );
+};
+
+export default ChosenSpeaker;
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -16,20 +16,19 @@ const Home = () => {
     return (
       <div className="container-fluid">
         <div className="row justify-content-center first-row">
-          <div className="col-md-8 col-lg-6 py-4">
+          <div className="home-text col-md-8 col-lg-6 py-4">
             <h3 className="welcome-message">Welcome!</h3>
             <p>This is the website for the Technology Conference 2024.</p>
             <p>This webpage provides useful information about the conference such as  guest speakers, talk information, times, and sessions!</p>
             <p>Use this website to create your personal itinerary and search for talks you may be interested in.</p>
             <p>We look forward to greeting you! </p> 
           </div>
-
           <GuestSpeakers speakers={speakers} setChosenSpeaker={setChosenSpeaker} />
-
         </div>
+        <div className="divider my-4"></div>
         <div className="row justify-content-center third-row">
           <div className="col-md-8 col-lg-6">
-            <h3>Talks by {chosenSpeaker ? chosenSpeaker: "select a speaker"}</h3>
+            <h3>Talks by {chosenSpeaker ? chosenSpeaker: "select a speaker ..."}</h3>
             {chosenSpeaker && (
               <ChosenSpeaker chosenSpeaker={chosenSpeaker} filteredTalks={filteredTalks} />
             )}
